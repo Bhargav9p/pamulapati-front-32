3ed350ab2de47a2c27b3b99a5c7398c8feae65aa
Bhargav9p <pamulapatib.243@gmail.com>
2023-06-20 09:38:33 -0500

    angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
+import { Route } from '@angular/router';
+
+
+
+
+
+@NgModule({
+  declarations: [											
+    AppComponent,
+      LoginComponent,
+      SignupComponent,
+      ProfileComponent,
+      HeaderComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
